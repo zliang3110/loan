ff4025d75cdce2c75eef6b0ca638e5634280392b
zhangll <zhangll@gaoweideMacBook-Pro.local>
2017-06-16 23:58:01 +0800

    初始化项目

---
+++ src/main/java/com/zhangll/loan/LoanApplication.java
@@ -0,0 +1,12 @@
+package com.zhangll.loan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanApplication.class, args);
+	}
+}
